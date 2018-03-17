1dde2a6bbb668ab2ae4430e33e9ba1e231bbd522
EllangoK <ellangok@bxscience.edu>
2018-03-17 14:48:51 -0400

    Started form work

---
+++ website/static/fresh.js
@@ -0,0 +1,42 @@
+$.fn.getFormData = function() {
+  var fields = this.find('[name]');
+  var result = {};
+  $.each(fields, function (i, el) {
+    result[el.name] = el.value;
+  });
+  return result;
+};
+var config = {
+  apiKey: "AIzaSyCmT7KU_k0dsm9s5oSyD7py8O-8owMw190",
+  authDomain: "food-store-delivery-database.firebaseapp.com",
+  databaseURL: "https://food-store-delivery-database.firebaseio.com",
+  projectId: "food-store-delivery-database",
+  storageBucket: "food-store-delivery-database.appspot.com",
+  messagingSenderId: "645899371811"
+};
+var fb = firebase.initializeApp(config);
+var user_id;
+$('.submit').on('click', function (e) {
+  if (document.getElementById("storename").value == "") {
+    alert("Please enter a store name");
+    return;
+  }
+  if (document.getElementById("productname").value == "") {
+    alert("Please enter a product name");
+    return;
+  }
+  if (document.getElementById("price").value == "") {
+    alert("Please enter the price");
+    return;
+  }
+  if (document.getElementById("stock").value == "") {
+    alert("Please enter the stock");
+    return;
+  }
+  e.preventDefault();
+  var updates = {};
+  user_id = user_id ? user_id : fb.database().ref().child('user_data').push().key;
+  updates[user_id] = $('form').getFormData();
+  var path = 'Supermarkets/' + document.getElementById("storename").value + '/Delivery/' + document.getElementById("productname").value;
+  fb.database().ref().child(path).update(updates);
+});
